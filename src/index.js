f4ba8607b64924a089ad10a509bf55344b510f68
Amal Augustine <37372480+iamal12@users.noreply.github.com>
2023-08-05 11:44:23 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,29 @@
-const express = require('express')
-
+const express = require('express');
 const app = express();
-const cors = require('cors')
+const cors = require('cors');
 const login = require('./routes/login');
 const customer = require('./routes/customer');
-const menu = require("./routes/menu");
-const delivery = require("./routes/delivery");
-const orderdb = require("./routes/orderdb");
-const cart = require("./routes/cart");
-const employee = require("./routes/employee");
+const menu = require('./routes/menu');
+const delivery = require('./routes/delivery');
+const orderdb = require('./routes/orderdb');
+const cart = require('./routes/cart');
+const employee = require('./routes/employee');
 
-//Settings
-const ipAddress = '10.0.13.229'; // Set your desired IP address here
-//Middlewares
+// Middlewares
 app.use(express.json());
 app.use(cors());
-const port =3000;
-//Routes
-app.use("/login", login);
-app.use("/customer", customer);
-app.use("/menu", menu);
-app.use("/orderdb", orderdb);
-app.use("/delivery", delivery);
-app.use("/cart", cart);
-app.use("/employee",employee);
 
-//Starting the server
-app.listen(5020,ipAddress, () => {
-    console.log('Server running on port 5020');
+// Routes
+app.use('/login', login);
+app.use('/customer', customer);
+app.use('/menu', menu);
+app.use('/orderdb', orderdb);
+app.use('/delivery', delivery);
+app.use('/cart', cart);
+app.use('/employee', employee);
+
+// Starting the server
+const port = 5020; // Set your desired port number here
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
